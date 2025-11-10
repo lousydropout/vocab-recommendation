62c3cfb02d1286d753846b4484154a14843edb8a
lousydropout <lousydropout@gmail.com>
2025-11-10 16:45:36 -0600

    Initial commit

---
+++ test/vocab_recommendation.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib/core';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as VocabRecommendation from '../lib/vocab_recommendation-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/vocab_recommendation-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new VocabRecommendation.VocabRecommendationStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
